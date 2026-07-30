de2f5bcba8053de68b52dd248dec34e66632f44a
sharana <sharana.sangeetha@gmail.com>
2026-07-30 18:49:06 +0530

    chages to the program

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,6 +5,8 @@ public class WelcomeToJava {
         System.out.println("Welcome to the Java Track");
         System.out.println("I already know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using Java.");
+        System.out.println("I am excited to start this journey!");
+        System.out.println("Let's start coding!");
 
     }
 }
