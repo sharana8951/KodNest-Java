45239c4cd5df722dda56b1fc16eb37b5cbdd2ba4
vinay <vinay@kodnest.com>
2026-07-30 18:26:19 +0530

    change

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("Welcome to the Java Track");
+        System.out.println("Welcome to the Java Track");
+        System.out.println("I already know how to build programming logic.");
+
     }
 }
