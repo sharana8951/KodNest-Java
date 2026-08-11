02849edde4766a60692664a1a04e2b62fe8d8a03
vinay <vinay@kodnest.com>
2026-08-11 15:23:46 +0530

    method

---
+++ track/M02/T01/ST01/P01/Methods/Pgm1.java
@@ -0,0 +1,11 @@
+
+class Pgm1 {
+
+    public static void main(String[] args) {
+        greet();
+    }
+
+    static void greet() {
+        System.out.println("Good Morning");
+    }
+}
---
+++ track/M02/T01/ST01/P01/Methods/Pgm2.java
@@ -0,0 +1,14 @@
+
+class Pgm2 {
+
+    public static void main(String[] args) {
+        add(10, 5);
+
+    }
+
+    static void add(int a, int b) {
+        int res = a + b;
+
+        System.out.println(res);
+    }
+}
