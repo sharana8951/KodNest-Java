e7bc5bc7533584820f25adbc6d408d18c9945e31
vinay <vinay@kodnest.com>
2026-08-12 15:40:15 +0530

    shadowing

---
+++ track/M02/T01/ST01/P01/MethodOverloading/AddApp.java
@@ -0,0 +1,12 @@
+
+class AddApp {
+
+    public static void main(String[] args) {
+
+        Addition al = new Addition();
+
+        int r1 = al.add(10, 20);
+
+        System.err.println(r1);
+    }
+}
